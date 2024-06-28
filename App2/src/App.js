6ca365eb068a424bf30f34469b23d3908b9af31c
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2024-06-28 10:17:32 +0700

    Add files via upload

---
+++ App2/src/App.js
@@ -0,0 +1,18 @@
+import List from "./List";
+import data from "./data";
+import React, { useState } from "react";
+function App() {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
